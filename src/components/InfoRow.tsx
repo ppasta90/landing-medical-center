0b66d6e5cfa54879275116f8aa0dfad168e9d2cb
Paolo Pastacaldi <paolo.pastacaldi@knobs.it>
2023-09-17 15:51:56 +0200

    feat: add components folder and list of components

---
+++ src/components/InfoRow.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const InfoRow = () => {
+  return <div>InfoRow</div>;
+};
+
+export default InfoRow;
---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+import Button from "./Button";
+import Card from "./Card";
+import Drawer from "./Drawer";
+import Footer from "./Footer";
+import Header from "./Header";
+import InfoRow from "./InfoRow";
+
+export { Button, Card, Drawer, Footer, Header, InfoRow };
