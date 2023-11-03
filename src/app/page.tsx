89c16ce96753660456a9866ee526cdeb36af5de7
Paolo Pastacaldi <paolo.pastacaldi@knobs.it>
2023-11-03 19:30:13 +0100

    feat: add header and useMedia hook, add drawer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@
 
 import React, { useEffect, useState } from "react";
 import styles from "./page.module.scss";
-import { Button } from "@/components";
 import { useTheme } from "next-themes";
+import Header from "@/components/Header/Header";
+import Button from "@/components/Button/Button";
 
 export default function Home() {
   const { theme, setTheme } = useTheme();
@@ -11,10 +12,16 @@ export default function Home() {
 
   useEffect(() => setMounted(true), []);
 
+  const [isOpen, setIsOpen] = React.useState(false);
+  const toggleDrawer = () => {
+    setIsOpen((prevState) => !prevState);
+  };
+
   if (!mounted) return null;
 
   return (
     <main className={styles.main}>
+      <Header isOpen={isOpen} toggleDrawer={toggleDrawer} />
       The current theme is: {theme}
       <Button
         onClick={() => setTheme("light")}
